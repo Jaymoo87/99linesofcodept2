5b7279c837b91e83d5b841928515446c34ffed31
Justin Murrah <jdmurrah@gmail.com>
2022-11-03 13:36:20 -0500

    names are in the 'friends' divs'

--- app.js
+++ app.js
@@ -2,27 +2,49 @@ document.addEventListener('DOMContentLoaded', function(){
 const friends = ['Jamie', 'Jen', 'Sequoya', 'Jeff', 'Logan'];
 
 let button = document.getElementById('singbutton');
-document.body.appendChild(button)
+document.body.appendChild(button);
 
-let Jamie = document.createElement('div')
-let Jen = document.createElement('div')
-let Sequoya = document.createElement('div')
-let Jeff = document.createElement('div')
-let Logan = document.createElement('div')
+let Jamie = document.createElement('div');
+let Jen = document.createElement('div');
+let Sequoya = document.createElement('div');
+let Jeff = document.createElement('div');
+let Logan = document.createElement('div');
 
  
-Jamie.className = 'friend'
-Jen.className = 'friend' 
-Sequoya.className = 'friend'
-Jeff.className = 'friend'
-Logan.className = 'friend'
+Jamie.className = 'friend';
+Jen.className = 'friend';
+Sequoya.className = 'friend';
+Jeff.className = 'friend';
+Logan.className = 'friend';
 
 
-document.body.appendChild(Jamie)
-document.body.appendChild(Jen)
-document.body.appendChild(Sequoya)
-document.body.appendChild(Jeff)
-document.body.appendChild(Logan)
+document.body.appendChild(Jamie);
+document.body.appendChild(Jen);
+document.body.appendChild(Sequoya);
+document.body.appendChild(Jeff);
+document.body.appendChild(Logan);
+
+let h3 = document.createElement('h3');
+
+let Jamieh3 = document.createElement('h3');
+Jamieh3.textContent = 'Jamie';
+Jamie.appendChild(Jamieh3);
+
+let Jenh3 = document.createElement('h3');
+Jenh3.textContent = 'Jen';
+Jen.appendChild(Jenh3);
+
+let Sequoyah3 = document.createElement('h3');
+Sequoyah3.textContent = 'Sequoya';
+Sequoya.appendChild(Sequoyah3);
+
+let Jeffh3 = document.createElement('h3');
+Jeffh3.textContent = 'Jeff';
+Jeff.appendChild(Jeffh3);
+
+let Loganh3 = document.createElement('h3');
+Loganh3.textContent = 'Logan';
+Logan.appendChild(Loganh3);
 
 
 
